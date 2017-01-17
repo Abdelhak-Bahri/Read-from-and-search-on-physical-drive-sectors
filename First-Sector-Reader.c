a15f600662e01e4933d82e756b3b46775424f086
BAHRI Abdelhak <ca_bahri@esi.dz>
2017-01-17 15:02:57 +0100

    deleting old unused functions

--- First-Sector-Reader.c
+++ First-Sector-Reader.c
@@ -1,58 +1,5 @@
 #include <stdio.h>
 
-int main2 (int argc, char **argv)
-{
-  /*
-    FILE *usb_ptr;
-    unsigned char buffer[512];
-    int i, j;
-
-    if (argc == 1)
-    {
-        printf ("wot no stick?\n");
-        //return -1;
-    }
-
-    //usb_ptr = fopen(argv[1],"rb");
-    usb_ptr = fopen("\\\\.\\D:", "rb+");
-    if(usb_ptr == NULL)
-    {
-        printf("error opening USB Drive for reading");
-    }
-
-    i = 0;
-    while (1)
-    {
-        if (fread (buffer, 512,1, usb_ptr) < 1)
-            break;
-        //printf("\n\n\t\t\t\tok");
-        i++;
-        if (!(i & 127))
-            printf ("%d sectors read..\r", i);
-        if (buffer[0] == 'S' && buffer[1] == 'o'&& buffer[2] == 'u'&& buffer[3] == 'i'&& buffer[4] == 'c'&& buffer[5] == 'i')
-        {
-            for (j=0; j<32; j++)
-                printf ("%02X ", buffer[j]);
-            printf ("| ");
-            for (j=0; j<32; j++)
-            {
-                if (buffer[j] >= ' ' && buffer[j] <= '~')
-                    printf ("%c", buffer[j]);
-                else
-                    printf (".");
-            }
-            printf ("\n");
-        }
-    }
-
-    fclose (usb_ptr);
-*/
-    return 0;
-}
-
-
-
-
 
 int lireSecteur(char* lettreDisque){
     unsigned char flux[512];
